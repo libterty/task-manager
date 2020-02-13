33919d6bc321abd888ab14d055ffcca8dbf3a7f2
dreak603 <dreak603@gmail.com>
2020-02-13 14:25:15 +0800

    add deleteTaskById

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -29,4 +29,8 @@ export class TasksService {
     this.tasks.push(task);
     return task;
   }
+
+  deleteTaskById(id: string): void {
+    this.tasks = this.tasks.filter(item => item.id !== id);
+  }
 }
