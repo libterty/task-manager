ea2bfb6f0d13d9ce3d2bb526c2f599161bab45f2
dreak603 <dreak603@gmail.com>
2020-02-14 13:27:47 +0800

    init typeorm setting with pg

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,3 +1,9 @@
+## Version 0.0.3 (2019-02-14)
+
+### New features
+
+- Adding Postgres connection and orm settings[``]
+
 ## Version 0.0.2 (2019-02-14)
 
 ### New features
--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,7 +1,6 @@
 import { TaskStatus } from '../task.model';
 import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
-
 export class GetTasksFilterDto {
   @IsOptional()
   @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
